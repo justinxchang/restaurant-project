efb6d915a813b8c42391e85832695d2ca4ffc030
Justin Chang <justinxchang@hotmail.com>
2018-12-05 16:22:25 -0700

    host

--- db/get_member_history.sql
+++ db/get_member_history.sql
@@ -3,3 +3,9 @@ From member m
 JOIN history h on h.member_id = m.member_id
 WHERE m.member_id = $1
 ORDER by h.order_num
+
+CREATE TABLE member_history (
+    id serial primary key,
+    member_id int references member(member_id) not null,
+    order_num int references history(order_num) not null
+)
--- src/components/Navbar2.js
+++ src/components/Navbar2.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
-import {connect} from 'react-redux'
-import {Navbar, Nav, NavItem} from 'react-bootstrap'
+import { connect } from 'react-redux'
+import { Navbar, Nav, NavItem } from 'react-bootstrap'
 import './Navbar.css'
 import axios from 'axios'
 
 class Navbar2 extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
 
         this.state = {
@@ -16,18 +16,18 @@ class Navbar2 extends Component {
 
     async componentDidMount() {
         let res = await axios.get('/api/user-data')
-        if(res.data){
-            this.setState({loggedIn: true})
-            console.log('res',res.data)
+        if (res.data) {
+            this.setState({ loggedIn: true })
+            console.log('res', res.data)
         }
     }
-    
-    render() {
-        if(this.state.loggedIn){
 
-        return (
-            <Nav pullRight>
-                {/* <NavItem eventKey={5} componentClass={Link} to='/addform' href="/addform">
+    render() {
+        if (this.state.loggedIn) {
+
+            return (
+                <Nav pullRight>
+                    {/* <NavItem eventKey={5} componentClass={Link} to='/addform' href="/addform">
                     Add Form
                 </NavItem>
                 <NavItem eventKey={6} componentClass={Link} to='/orders' href="/orders">
@@ -36,23 +36,30 @@ class Navbar2 extends Component {
                 <NavItem eventKey={7} componentClass={Link} to='/chart' href="/chart">
                     Chart
                 </NavItem> */}
-                <NavItem eventKey={8} componentClass={Link} to='/profile' href="/profile">
-                    Profile
-                </NavItem>
-            </Nav>
-        )
-         
-    } else {
-        return (
-            <Nav pullRight>
-
-            <NavItem eventKey={4} componentClass={Link} to='/login' href="/login">
-                Login
+                    <NavItem eventKey={4} componentClass={Link} to='/login' href="/login">
+                        Login
             </NavItem>
-        </Nav>
-        )
+                    <NavItem eventKey={8} componentClass={Link} to='/profile' href="/profile">
+                        Profile
+                </NavItem>
+
+                </Nav>
+            )
+
+        } else {
+            return (
+                <Nav pullRight>
+
+                    <NavItem eventKey={4} componentClass={Link} to='/login' href="/login">
+                        Login
+            </NavItem>
+                    <NavItem eventKey={8} componentClass={Link} to='/profile' href="/profile">
+                        Profile
+                </NavItem>
+                </Nav>
+            )
+        }
     }
-}
 }
 
 export default Navbar2;
